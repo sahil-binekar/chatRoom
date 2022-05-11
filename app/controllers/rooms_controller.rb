6a28f11ef2d67d717c1a3104789b8dbef6982a67
sahil-binekar <sahil.binekar@cryptextechnologies.com>
2022-05-11 17:35:42 +0530

    added images and css

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,5 +5,10 @@ class RoomsController < ApplicationController
     redirect_to '/signin' unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.create(name: params["room"]["name"])
   end
 end
